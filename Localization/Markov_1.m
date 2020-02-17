2ce59c711cf8e3207577ee050fb0d3ee3fbdc086
JAYA SURYA M <57759507+uknwho@users.noreply.github.com>
2020-02-17 18:41:40 +0530

    Markov_localization

---
+++ Localization/Markov_1.m
@@ -0,0 +1,13 @@
+pTransition = [ 
+    0   0   0   0   0
+    0  .05  .1 .05  0
+    0   .1  .4  .1  0
+    0  .05  .1 .05  0
+    0   0   0   0   0  ];
+getPLocalTransition = @(localCoordinate) ...
+    pTransition(localCoordinate(1) + 3, localCoordinate(2) + 3);
+%%%%%%%%%%% PLEASE DON'T CHANGE ANYTHING ABOVE THIS LINE %%%%%%%%%%%
+
+calcPTransition = @(x_t, u_t, x_tm1) getPLocalTransition(min([2,2],max([-2,-2],x_t-(x_tm1+u_t)))); % maybe use getPLocalTransition
+
+plotPredictionUpdate(calcPTransition);
